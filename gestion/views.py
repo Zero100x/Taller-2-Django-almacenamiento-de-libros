03266118dffb47c9e95f63798a92063c59123443
william <william.inzandara@campusucc.edu.co>
2026-04-23 09:24:48 -0500

    feat: add forms for autor and libro

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Autor, Libro
+from .forms import AutorForm, LibroForm
 
-# Create your views here.
+# AUTORES
+class AutorListView(ListView):
+    model = Autor
+    template_name = 'gestion/autor_list.html'
+    context_object_name = 'autores'
+
+class AutorCreateView(CreateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'gestion/autor_create.html'
+    success_url = reverse_lazy('autor_list')
+
+class AutorUpdateView(UpdateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'gestion/autor_update.html'
+    success_url = reverse_lazy('autor_list')
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = 'gestion/autor_delete.html'
+    success_url = reverse_lazy('autor_list')
+
+# LIBROS
+class LibroListView(ListView):
+    model = Libro
+    template_name = 'gestion/libro_list.html'
+    context_object_name = 'libros'
+
+class LibroCreateView(CreateView):
+    model = Libro
+    form_class = LibroForm
+    template_name = 'gestion/libro_create.html'
+    success_url = reverse_lazy('libro_list')
+
+class LibroUpdateView(UpdateView):
+    model = Libro
+    form_class = LibroForm
+    template_name = 'gestion/libro_update.html'
+    success_url = reverse_lazy('libro_list')
+
+class LibroDeleteView(DeleteView):
+    model = Libro
+    template_name = 'gestion/libro_delete.html'
+    success_url = reverse_lazy('libro_list')
